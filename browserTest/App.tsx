a53336efb50fb74a90f19c93526ce5a54936b671
kunchtler <leo.kulinski@gmail.com>
2025-04-23 19:11:54 +0200

    Added mantine for React UI

--- browserTest/App.tsx
+++ browserTest/App.tsx
@@ -36,3 +36,11 @@ function App() {
 }
 
 export default App;
+
+// import "@mantine/core/styles.css";
+// import { MantineProvider } from "@mantine/core";
+// import { theme } from "./theme";
+
+// export default function App() {
+//     return <MantineProvider theme={theme}>App</MantineProvider>;
+// }
