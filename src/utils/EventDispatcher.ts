3cf37cd4e5bc9d5c0ed0bf554bdb07892d3b70ca
kunchtler <leo.kulinski@gmail.com>
2025-04-30 20:00:41 +0200

    Added class to disptach events in JS

---
+++ src/utils/EventDispatcher.ts
@@ -0,0 +1,63 @@
+export type CallbackFunction = (...args: any[]) => void;
+
+// TODO : Use callback or eventListener as variable names / in doc ?
+
+/**
+ * Implements the Observer pattern to add and remove event listeners (akin to the DOM events but for JS scripts).
+ */
+export class EventDispatcher {
+    _listeners: Map<string, CallbackFunction[]> = new Map();
+
+    /**
+     * Adds a function to be callbacked when the event will be dispatched.
+     * @param eventName The name of the event to react to.
+     * @param callback The callback function.
+     * @returns A function to later remove this event listener.
+     */
+    addEventListener(eventName: string, callback: CallbackFunction): () => void {
+        let callbacks = this._listeners.get(eventName);
+        if (callbacks === undefined) {
+            callbacks = [];
+            this._listeners.set(eventName, callbacks);
+        }
+        callbacks.push(callback);
+        return () => {
+            this.removeEventListener(eventName, callback);
+        };
+    }
+
+    /**
+     * Removes a callback from a given event.
+     * @param eventName The name of the event to be removed from.
+     * @param callback The callback function to remove (must be the same callback that was added with addEventListener)
+     */
+    private removeEventListener(eventName: string, callback: CallbackFunction): void {
+        let callbacks = this._listeners.get(eventName);
+        if (callbacks === undefined) {
+            return;
+        }
+        callbacks = callbacks.filter((value) => value !== callback);
+        // Remove the event name if it has no more listeners.
+        if (callbacks.length === 0) {
+            this._listeners.delete(eventName);
+        }
+    }
+
+    /**
+     * Dispatch an event by calling any callback function listening to this event.
+     * @param eventName The name of the event to dispatch.
+     * @param args The arguments to pass to each callback.
+     */
+    dispatchEvent(eventName: string, ...args: any[]): void {
+        this._listeners.get(eventName)?.forEach((callback) => {
+            callback(...args);
+        });
+    }
+
+    /**
+     * Remove all event listeners.
+     */
+    destroy(): void {
+        this._listeners = new Map();
+    }
+}
