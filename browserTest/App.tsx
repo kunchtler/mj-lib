7733767019f1897b0aa9bf0eec9924fb3f00bf5b
kunchtler <leo.kulinski@gmail.com>
2025-09-05 18:44:59 +0200

    Externalized map functions from App

--- browserTest/App.tsx
+++ browserTest/App.tsx
@@ -5,13 +5,13 @@ import { BasicJuggler } from "../src/react/examples/BasicJuggler";
 import { BasicJugglerProps } from "../src/react/mesh/JugglerMesh";
 import { BasicTable, BasicTableProps } from "../src/react/examples/BasicTable";
 import { ballVelocity, Clock, TossCatchEvent, TossEvent } from "../src";
-import { useEffect, useRef, useState } from "react";
+import { RefObject, useEffect, useRef, useState } from "react";
 import { TimeControls } from "./TimeControls";
 import { PerformanceModel } from "../src/model/PerformanceModel";
 import { PerformanceView } from "../src/view/PerformanceView";
 import * as THREE from "three";
 import { pattern } from "./pattern";
-import { patternToModel } from "../src/inference/PatternToModel";
+import { JugglingPatternRaw, patternToModel } from "../src/inference/PatternToModel";
 import { OrbitControls, TorusKnot } from "@react-three/drei";
 import styles from "./simulator.module.css";
 import mergeRefs from "merge-refs";
@@ -20,34 +20,28 @@ extend(LineMaterial);
 //TODO : styles ?
 //TODO : clock optional for performance ?
 
-const ballsData: BasicBallProps[] = [
-    { id: "Do?K", color: "red" },
-    { id: "Re?K", color: "orange" },
-    { id: "Mi?K", color: "yellow" }
-];
+// TODO : Rename "model" to events ???
 
-const model = patternToModel(pattern);
+type performanceDescription = {
+    ballsData: { id: string; color: THREE.ColorRepresentation }[];
+    model: PerformanceModel;
+    jugglersData: { name: string; position: THREE.Vector3Tuple; rotation?: THREE.Vector3Tuple }[];
+    tablesData: { name: string; position: THREE.Vector3Tuple; rotation?: THREE.Vector3Tuple }[];
+};
+
+const description: performanceDescription = {
+    model: patternToModel(pattern),
+    ballsData: [
+        { id: "Do?K", color: "red" },
+        { id: "Re?K", color: "orange" },
+        { id: "Mi?K", color: "yellow" }
+    ],
+    jugglersData: [{ name: "Kylian", position: [-1, 0, 0] }],
+    tablesData: [{ name: "KylianT", position: [0, 0, 0], rotation: [0, Math.PI, 0] }]
+};
 const clock = new Clock();
-const jugglersData: BasicJugglerProps[] = [
-    { name: "Kylian", position: [-1, 0, 0] as [number, number, number] }
-];
-const tablesData: BasicTableProps[] = [
-    { name: "KylianT", position: [0, 0, 0], rotation: [0, Math.PI, 0] }
-];
 
 export function App() {
-    const [jugglersData] = useState<BasicJugglerProps[]>([
-        { name: "Kylian", position: [-1, 0, 0] as [number, number, number] }
-    ]);
-    const [tablesData] = useState<BasicTableProps[]>([
-        { name: "KylianT", position: [0, 0, 0], rotation: [0, Math.PI, 0] }
-    ]);
-
-    const mesh = new THREE.Mesh(
-        new THREE.TorusGeometry(),
-        new THREE.MeshLambertMaterial({ color: "red" })
-    );
-
     return (
         <>
             <Canvas frameloop="always" camera={{ position: [3, 2, 0] }}>
@@ -57,15 +51,7 @@ export function App() {
                 <directionalLight args={[0xfefded, 1]} />
                 <axesHelper args={[1.5]} position={[0, 0.01, 0]} />
                 <gridHelper args={[30, 30]} />
-                <primitive object={mesh} />
-                <CanvasContent
-                    clock={clock}
-                    model={model}
-                    ballsData={ballsData}
-                    jugglersData={jugglersData}
-                    tablesData={tablesData}
-                    mesh={mesh}
-                />
+                <CanvasContent />
             </Canvas>
             <div className={styles.timecontrols}>
                 <TimeControls clock={clock} />
@@ -74,22 +60,10 @@ export function App() {
     );
 }
 
-function CanvasContent({
-    clock,
-    model,
-    ballsData,
-    jugglersData,
-    tablesData,
-    mesh
-}: {
-    clock: Clock;
-    model: PerformanceModel;
-    ballsData: BasicBallProps[];
-    jugglersData: BasicJugglerProps[];
-    tablesData: BasicTableProps[];
-    mesh: THREE.Mesh;
-}) {
-    const [performance] = useState(() => new PerformanceView({ model: model, clock: clock }));
+function CanvasContent() {
+    const [performance] = useState(
+        () => new PerformanceView({ model: description.model, clock: clock })
+    );
     const ballsRef = useRef(new Map<string, THREE.Object3D>());
     const jugglersRef = useRef(
         new Map<string, { leftHand: THREE.Object3D | null; rightHand: THREE.Object3D | null }>()
@@ -97,8 +71,6 @@ function CanvasContent({
 
     useFrame(() => {
         const time = performance.getClock().getTime();
-
-        mesh.position.setY(Math.sin(time));
 
         // Update the balls' positions.
         for (const [id, { model }] of performance.balls) {
@@ -129,129 +101,89 @@ function CanvasContent({
         // }
     });
 
-    const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
-
-    // useEffect(() => {
-    //     const tmp = setTimeout(() => {
-    //         const event = model.balls.get("Do?K")?.timeline.nextEvent(0)[1];
-    //         if (event instanceof TossCatchEvent) {
-    //             console.log(`Example of fetching information at time ${event.time}:`);
-    //             console.log(`Is event a toss ? : ${event instanceof TossEvent}`);
-    //             console.log(`Siteswap height : ${event.siteswapHeight}`);
-    //             console.log(`Time between two successive toss of the juggler : ${event.unitTime}s`);
-    //             const tossPerMinute = 60 / event.unitTime;
-    //             console.log(`Number of tosses per minute of the juggler : ${tossPerMinute}`);
-    //             const tossStartPos = event.ball.positionAtEvent(event);
-    //             const tossEndPos = event.ball.positionAtEvent(event.nextBallEvent()[1]);
-    //             console.log(`Toss starting position : ${tossStartPos.toArray()}`);
-    //             console.log(`Toss ending position : ${tossEndPos.toArray()}`);
-    //             console.log(
-    //                 "At that tempo, if the starting and ending positions of the toss are the same, here are the velocities the ball should have when tossed at different siteswap heights :\nTODO : Technically, this doesn't take into account the time the ball spends in hand... In order to fix this, interact with the timeline at the event layer, and have dwell time parameter there ?\nTODO : This won't work if the juggler's tempo changes midflight."
-    //             );
-    //             for (let i = 1; i < 7; i++) {
-    //                 const flightTime = event.unitTime * i;
-    //                 console.log(
-    //                     `Siteswap height ${i} :\n\tFlight time ${flightTime}\n\tVelocity vector ${ballVelocity(tossStartPos, 0, tossEndPos, flightTime, 0).toArray()}`
-    //                 );
-    //             }
-    //         }
-    //     }, 1000);
-    // }, [model]);
-
-    function mapBalls({ id, ref, ...props }: BasicBallProps) {
-        return (
-            <BasicBall
-                id={id}
-                key={id}
-                ref={mergeRefs((elem) => {
-                    if (elem === null) {
-                        ballsRef.current.delete(id);
-                    } else {
-                        ballsRef.current.set(id, elem);
-                    }
-                    /*@ts-expect-error React 19's refs are weirdly typed*/
-                }, ref)}
-                {...props}
-            />
-        );
-    }
-    // <BasicBall
-    //     id="Do?K"
-    //     color="red"
-    //     ref={(elem) => {
-    //         if (elem !== null) {
-    //             ballsRef.current.set("Do?K", elem);
-    //         }
-    //     }}
-    // />
-
-    function mapJuggler({ name, ...props }: BasicJugglerProps) {
-        return (
-            <BasicJuggler
-                name={name}
-                key={name}
-                rightHandRef={(elem) => {
-                    const ref = jugglersRef.current.get(name);
-                    if (ref === undefined) {
-                        if (elem !== null) {
-                            jugglersRef.current.set(name, {
-                                rightHand: elem,
-                                leftHand: null
-                            });
-                        }
-                    } else {
-                        ref.rightHand = elem;
-                        if (ref.rightHand === null && ref.leftHand === null) {
-                            // jugglersRef.current.delete(name);
-                        }
-                    }
-                }}
-                leftHandRef={(elem) => {
-                    const ref = jugglersRef.current.get(name);
-                    if (ref === undefined) {
-                        if (elem !== null) {
-                            jugglersRef.current.set(name, {
-                                rightHand: null,
-                                leftHand: elem
-                            });
-                        }
-                    } else {
-                        ref.leftHand = elem;
-                        if (ref.rightHand === null && ref.leftHand === null) {
-                            // jugglersRef.current.delete(name);
-                        }
-                    }
-                }}
-                {...props}
-            />
-        );
-    }
-    // <BasicJuggler
-    //     name="Kylian"
-    //     position={[-1, 0, 0]}
-    //     rightHandRef={(elem) => {
-    //         const ref = jugglersRef.current.get("Kylian");
-    //         if (ref !== undefined) {
-    //             ref.rightHand = elem;
-    //         }
-    //     }}
-    //     leftHandRef={(elem) => {
-    //         const ref = jugglersRef.current.get("Kylian");
-    //         if (ref !== undefined) {
-    //             ref.leftHand = elem;
-    //         }
-    //     }}
-    // />
-
-    function mapTables({ name, ...props }: BasicTableProps) {
-        return <BasicTable name={name} key={name} {...props} />;
-    }
-
     return (
         <Performance audio={true} clock={clock} performance={performance}>
-            {jugglersData.map((elem) => mapJuggler(elem))}
-            {tablesData.map((elem) => mapTables(elem))}
-            {ballsData.map((elem) => mapBalls(elem))}
+            {description.jugglersData.map((elem) => mapJuggler(elem, jugglersRef))}
+            {description.tablesData.map((elem) => mapTables(elem))}
+            {description.ballsData.map((elem) => mapBalls(elem, ballsRef))}
         </Performance>
     );
+}
+
+function mapBalls(
+    { id, ref, ...props }: BasicBallProps,
+    ballsRef: RefObject<Map<string, THREE.Object3D>>
+) {
+    return (
+        <BasicBall
+            id={id}
+            key={id}
+            ref={mergeRefs((elem) => {
+                if (elem === null) {
+                    ballsRef.current.delete(id);
+                } else {
+                    ballsRef.current.set(id, elem);
+                }
+                /*@ts-expect-error React 19's refs are weirdly typed*/
+            }, ref)}
+            {...props}
+        />
+    );
+}
+
+function mapJuggler(
+    { name, ...props }: BasicJugglerProps,
+    jugglersRef: RefObject<
+        Map<
+            string,
+            {
+                leftHand: THREE.Object3D | null;
+                rightHand: THREE.Object3D | null;
+            }
+        >
+    >
+) {
+    return (
+        <BasicJuggler
+            name={name}
+            key={name}
+            rightHandRef={(elem) => {
+                const ref = jugglersRef.current.get(name);
+                if (ref === undefined) {
+                    if (elem !== null) {
+                        jugglersRef.current.set(name, {
+                            rightHand: elem,
+                            leftHand: null
+                        });
+                    }
+                } else {
+                    ref.rightHand = elem;
+                    if (ref.rightHand === null && ref.leftHand === null) {
+                        // jugglersRef.current.delete(name);
+                    }
+                }
+            }}
+            leftHandRef={(elem) => {
+                const ref = jugglersRef.current.get(name);
+                if (ref === undefined) {
+                    if (elem !== null) {
+                        jugglersRef.current.set(name, {
+                            rightHand: null,
+                            leftHand: elem
+                        });
+                    }
+                } else {
+                    ref.leftHand = elem;
+                    if (ref.rightHand === null && ref.leftHand === null) {
+                        // jugglersRef.current.delete(name);
+                    }
+                }
+            }}
+            {...props}
+        />
+    );
+}
+
+function mapTables({ name, ...props }: BasicTableProps) {
+    return <BasicTable name={name} key={name} {...props} />;
 }
