1b73297a64b10a972ac9e090c9b5c8a7640dd18d
kunchtler <leo.kulinski@gmail.com>
2025-10-03 18:33:15 +0200

    Created Test pattern

---
+++ src/examples/patternTest.ts
@@ -0,0 +1,46 @@
+import { JSONJugglingScore } from "../inference/PerformanceDescription";
+
+export const score: JSONJugglingScore = {
+    ballTemplates: [
+        {
+            name: "Do"
+        },
+        {
+            name: "Re"
+        },
+        {
+            name: "Mi"
+        }
+    ],
+    tableTemplates: [
+        {
+            name: "Piano",
+            spots: [
+                {
+                    name: "DoSpot1",
+                    acceptedBallName: "Do"
+                },
+                {
+                    name: "DoSpot2",
+                    acceptedBallName: "Do"
+                },
+                {
+                    name: "ReSpot",
+                    acceptedBallName: "Re"
+                },
+                {
+                    name: "MiSpot",
+                    acceptedBallName: "Mi"
+                }
+            ]
+        }
+    ],
+    scoreConverter: [{ bar: 0, tempo: { bpm: 180, note: 1 }, timeSignature: "4/4" }],
+    jugglers: [
+        {
+            name: "Vincent",
+            ballsHeldAtStart: [[{ name: "Do" }, { name: "Mi" }], [{ name: "Re" }]],
+            jugglingPhrases: [{ startTime: 0, pattern: "L3(3^100)", withTempo: 1 }]
+        }
+    ]
+};
