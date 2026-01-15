c66bbf7950431e16f394c82fa0f23f4132b17703
kunchtler <leo.kulinski@gmail.com>
2026-01-15 20:14:35 +0100

    Added support for correct matching with readonly structures

--- src/utils/utilityTypes.ts
+++ src/utils/utilityTypes.ts
@@ -15,22 +15,22 @@ export type DeepPartial<T> = Partial<{
  * It allows to combine the types of two tuples, element per element,
  * without losing the typescript fact that the resulted tuple is still tuple.
  */
-type MergeTuples<A, B> = A extends []
+type MergeTuples<A, B> = A extends readonly []
     ? B
-    : B extends []
+    : B extends readonly []
       ? A
-      : A extends [infer A0, ...infer ARest]
-        ? B extends [infer B0, ...infer BRest]
+      : A extends readonly [infer A0, ...infer ARest]
+        ? B extends readonly [infer B0, ...infer BRest]
             ? [DeepFuse<A0, B0>, ...MergeTuples<ARest, BRest>]
             : never
         : never;
 
-
 // TODO : Do shallow version, and have the deep version call the shallow one ?
+// TODO : This version works with readonlys, but it erases them from the output.
 /**
  * This type is scary, but don't be afraid. It functions as a type intersection, but it
  * tries to "fuse" object types.
- * For instane, combining types {x: number}[] and {y: string}[] yields
+ * For instance, combining types {x: number}[] and {y: string}[] yields
  * ({x: number} & {y: string})[] = {x: number; y: string}[]
  * (where it would normally yield ({x: number} | {y: string})[])
  * It does the same for tuples, sets, maps and of course arrays, and DOES
@@ -55,18 +55,18 @@ export type DeepFuse<A, B> =
           : // Combine arrays or tuples.
             A extends readonly (infer Aitem)[]
             ? B extends readonly (infer Bitem)[]
-                ? // Check if A and B are tuples first, as tuples are a special kind of array.
-                  number extends A["length"]
-                    ? number extends B["length"]
-                        ? // Both A and B are tuples, we combine, per-element, their types.
-                          MergeTuples<A, B>
-                        : // A is a tuple but B is an array.
+                ? // Check if A and B are tuples or arrays.
+                  number extends A["length"] // Is A an array ?
+                    ? number extends B["length"] // A is an array. Is B an array ?
+                        ? // Both A and B are arrays, combine the types of their elements.
+                          DeepFuse<Aitem, Bitem>[]
+                        : // A is an array but B is a tuple.
                           never
-                    : number extends B["length"]
-                      ? // A is an array but B is a tuple.
+                    : number extends B["length"] // A is a tuple. Is B an array ?
+                      ? // A is a tuple but B is an array.
                         never
-                      : // Both A and B are arrays, combine the types of their elements.
-                        DeepFuse<Aitem, Bitem>[]
+                      : // Both A and B are tuples, we combine, per-element, their types.
+                        MergeTuples<A, B>
                 : // A is an array or tuple, but B is not.
                   never
             : // Combine objects.
@@ -91,5 +91,24 @@ export type DeepFuse<A, B> =
               : // Combine types of A and B, which should be primitives.
                 A & B;
 
-// type Test0 = DeepMergeStrict<{ x: number }, { x: number; y: string }>;
-// type Test1 = DeepMergeStrict<Map<string, number>, Map<string, string>>;
+// type Xobj = {x: number}
+// type Yobj = {y: string}
+// type Test0 = DeepFuse<Xobj, Yobj>;
+// type Test1 = DeepFuse<Map<string, Xobj>, Map<string, Yobj>>;
+// type Test2 = DeepFuse<Set<Xobj>, Set<Yobj>>
+// type Test3 = DeepFuse<Xobj[], Yobj[]>
+// type Test4 = DeepFuse<[Xobj, Yobj], [Yobj]>
+// type Test5 = DeepFuse<[Xobj], []>
+// type A = {
+//     ballTemplates: {
+//         name: string;
+//     }[];
+// };
+// type Test3 = DeepFuse<A["ballTemplates"], A["ballTemplates"]>;
+
+// const x: (readonly number[])[] = [
+//     [1, 2, 3],
+//     [4, 5]
+// ];
+// x.push([2, 3]);
+// x[0].push(0);
