c3b0d9b9cf006d197c6d78c668a1d0472348538a
kunchtler <leo.kulinski@gmail.com>
2026-03-09 16:15:25 +0100

    Removed table templates from helper

--- src/inference/PerformanceDescriptionHelpers.ts
+++ src/inference/PerformanceDescriptionHelpers.ts
@@ -1,7 +1,7 @@
-import { BallSoundDescription } from "../model";
+import { DeepFuse } from "../utils";
 import {
+    BallSoundDescription,
     BallDescription,
-    BodyMeshDescription,
     ColorDescription,
     FractionDescription,
     GlobalBeatDescription,
@@ -10,6 +10,11 @@ import {
     JugglingPhrase,
     SpotDescription
 } from "./PerformanceDescription";
+
+// export type PerformanceDescriptionHelper = DeepFuse<
+//     DeepFuse<JugglingScoreHelper, PerformanceLayoutHelper>,
+//     PerformanceMeshDefinitionsHelper
+// >;
 
 export type JugglingScoreHelper = {
     version: "0.1";
@@ -20,24 +25,19 @@ export type JugglingScoreHelper = {
     }[];
     jugglers: {
         name: string;
-        table?: {
-            template: string;
-            ballsOnTableAtStart?: {
-                name: string;
-                id?: string | undefined;
-                spot?: string | undefined;
-            }[];
-        };
         ballsHeldAtStart?: [(BallDescription | undefined)[], (BallDescription | undefined)[]];
         beatReference?: Partial<JugglerBeatReference>;
         jugglingPhrases?: JugglingPhrase[];
+        defaultTableID?: string;
     }[];
-    tableTemplates?: {
-        name: string;
+    tables?: {
+        id: string;
         spots: {
             name: string;
             acceptedBallName: string; // TODO : Support undefined acceptedBallName ?
+            ball?: boolean | { name: string; id?: string };
         }[];
+        unknownSpot?: { balls: { name: string; id?: string }[] };
     }[];
     globalBeat?:
         | {
@@ -49,32 +49,37 @@ export type JugglingScoreHelper = {
         | ({ type: "variable" } & Partial<GlobalBeatDescription>);
 };
 
-export type PerformanceLayoutHelper = {
+// Both Meshes and Layout are described here, as the dimensions of the layout are quite linked to the helpers.
+export type PerformanceLayoutAndMeshHelper = {
     version: "0.1";
     ballTemplates: {
         name: string;
-        soundOnCatch?: BallSoundDescription; //TODO : Remove ?
-        soundOnToss?: BallSoundDescription; //TODO : Remove ?
+        // soundOnCatch?: BallSoundDescription; //TODO : Remove ?
+        // soundOnToss?: BallSoundDescription; //TODO : Remove ?
+        color?: ColorDescription;
+        radius?: number;
     }[];
     jugglers: {
         name: string;
         position?: [number, number, number];
         rotation?: [number, number, number];
         scale?: [number, number, number];
-        handBuilder?: HandLayoutHelper;
-        body?: { height?: number; depth?: number; width?: number };
-        table?: {
-            template: string;
-            position?: [number, number, number];
-            rotation?: [number, number, number];
-            scale?: [number, number, number];
+        handBuilder?: JugglerHandBuilder;
+        body?: {
+            height?: number;
+            depth?: number;
+            width?: number;
+            color?: ColorDescription;
+            visible?: boolean;
         };
+        defaultTableID?: string;
     }[];
-    tableTemplates?: {
-        name: string;
+    tables?: {
+        id: string;
         height?: number;
         width?: number;
         depth?: number;
+        scale?: [number, number, number];
         spots: {
             name: string;
             position: [number, number, number];
@@ -84,50 +89,50 @@ export type PerformanceLayoutHelper = {
             position: [number, number, number];
             rotation?: [number, number, number];
         };
-    }[];
-};
-
-export type PerformanceMeshDefinitionsHelper = {
-    ballTemplates: {
-        name: string;
+        position?: [number, number, number];
+        rotation?: [number, number, number];
         color?: ColorDescription;
-        radius?: number;
-    }[];
-    jugglers: {
-        name: string;
-        handBuilder?: {
-            // Mesh related properties.
-            length?: number; // From wrist to fingertip.
-            width?: number; // From thumb to little finger.
-            depth?: number; // From palm to back.
-            visible?: boolean; //P
-        };
-        body: {
-            height?: number;
-            width?: number;
-            depth?: number;
-            visible?: boolean;
-            color?: ColorDescription;
-        };
-        table?: {
-            template: string;
-            visible?: boolean;
-            color?: ColorDescription;
-        };
-    }[];
-    tableTemplates?: {
-        name: string;
-        height?: number;
-        width?: number;
-        depth?: number;
+        visible?: boolean;
     }[];
 };
 
-export type HandLayoutHelper = {
+// export type PerformanceMeshDefinitionsHelper = {
+//     ballTemplates: {
+//         name: string;
+//         color?: ColorDescription;
+//         radius?: number;
+//     }[];
+//     jugglers: {
+//         name: string;
+//         handBuilder?: {
+//             // Mesh related properties.
+//             length?: number; // From wrist to fingertip.
+//             width?: number; // From thumb to little finger.
+//             depth?: number; // From palm to back.
+//             visible?: boolean; //P
+//         };
+//         body: {
+//             height?: number;
+//             width?: number;
+//             depth?: number;
+//             visible?: boolean;
+//             color?: ColorDescription;
+//         };
+//     }[];
+//     tables?: {
+//         height?: number;
+//         width?: number;
+//         depth?: number;
+//         visible?: boolean;
+//         color?: ColorDescription;
+//     }[];
+// };
+
+export type JugglerHandBuilder = {
     length?: number; // From wrist to fingertip.
     width?: number; // From thumb to little finger.
     depth?: number; // From palm to back.
-    heldSpots: SpotDescription[]; // Relative to hand's wrist (x is towards thumb, y towards up, z towards fingers).
+    heldSpots?: SpotDescription[]; // Relative to hand's wrist (x is towards thumb, y towards up, z towards fingers).
     scale?: [number, number, number];
     spotsBuild?: {
         catchTossDistance?: number;
@@ -135,4 +140,6 @@ export type HandLayoutHelper = {
         distanceToMirroringLine?: number;
         jugglingPlaneDistanceFromJuggler?: number;
     };
+    visible?: boolean;
+    color?: ColorDescription;
 };
