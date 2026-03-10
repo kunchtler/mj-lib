1559c90fff5bfa43fc2d5d1c9f67b011f2e6ea83
kunchtler <leo.kulinski@gmail.com>
2026-03-10 01:25:32 +0100

    Updated deps

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig((config) => {
     // console.log(config);
     return {
         base: "",
-        server: { https: { key: "./musjugvr.key", cert: "./musjugvr.crt" } },
+        // server: { https: { key: "./musjugvr.key", cert: "./musjugvr.crt" } },
         build: {
             lib: {
                 entry: "./src/index.ts",
